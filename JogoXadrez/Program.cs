5a0ec225b0315203aeb377c293bcfb9d418dd08d
Lucas <lucas_eduardoksilva@hotmail.com>
2021-12-21 20:02:43 -0300

    Criando PosicaoXadrez

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -7,6 +7,10 @@ namespace JogoXadrez
     {
         static void Main(string[] args)
         {
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
             try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
