200c4782750e621da8b3ecbe9fe905698450e03d
Lucas <lucas_eduardoksilva@hotmail.com>
2021-12-21 19:47:57 -0300

    Adicionando pecas

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using tabuleiro;
-
+using xadrez;
 namespace JogoXadrez
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Posicao P;
             Tabuleiro tab = new Tabuleiro(8,8);
 
-            P = new Posicao(3, 4);
+            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            Console.WriteLine(P);
+            Tela.imprimirTabuleiro(tab);
+
             Console.ReadLine();
         }
     }
--- JogoXadrez/tabuleiro/Tabuleiro.cs
+++ JogoXadrez/tabuleiro/Tabuleiro.cs
@@ -16,6 +16,14 @@ namespace tabuleiro
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
         }
-
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
+        }
+        public void colocarPeca(Peca p, Posicao pos)
+        {
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
+        }
     }
 }
