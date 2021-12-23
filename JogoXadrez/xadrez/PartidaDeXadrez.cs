0c907b9b34a4f310021ca04e38f09d42ac971e1e
Lucas <lucas_eduardoksilva@hotmail.com>
2021-12-23 16:47:46 -0300

    Jogada especial Promocao

--- JogoXadrez/xadrez/PartidaDeXadrez.cs
+++ JogoXadrez/xadrez/PartidaDeXadrez.cs
@@ -140,6 +140,20 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque.");
             }
+            Peca p = tab.peca(destino);
+
+            // Jogada especial Promoção
+            if(p is Peao)
+            {
+                if((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
 
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
@@ -158,8 +172,6 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino);
 
             // Jogada especial En Passant
             if(p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
